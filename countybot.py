a582941101f14dfae93087201968981ebd54928f
Elvis Lutta <eolutta2@gmail.com>
2017-10-07 02:52:43 +0300

    Testing AfricasTalking USSD API

--- countybot.py
+++ countybot.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
 from flask_bootstrap import Bootstrap
 from flask_script import Manager
 
@@ -11,6 +11,31 @@ manager = Manager(app)
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
+
+@app.route('/ussd-pay', methods=['GET', 'POST'])
+def ussd_endpoint():
+    if request.method == 'POST':
+        text = request.args['text']
+        phonenumber = request.args['phonenumber']
+
+        if text == ''
+            response = 'CON Welcome to CountyBot how may we help you\n'
+            response += '1. Register'
+            response += '2. Make Payment'
+            response += '3. Change Location'
+
+        else if text == '1':
+            response = 'CON Enter your name'
+
+        else if text == '2':
+            response == 'CON Enter amount payment'
+
+        elif text == '3':
+            response == 'CON Enter your new locaion.\n Additional charges will be incurred.'
+
+        return response
+
 
 
 if __name__ == '__main__':
